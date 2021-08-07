b716d82fc8a9610581ac2707d20b3017e008d88b
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-08-07 23:00:19 +0530

    Preventing Minifiers from breaking application

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -3,30 +3,30 @@
 // Link this controller onto View sections by ng-controller="StudentController1"
 
 //Option 1 (Utilizing global varible myApp)
-myApp.controller('StudentController1', function($scope) {
+myApp.controller('StudentController1', ['$scope', function($scope) {
     $scope.intro = "Option1";
 
     $scope.btnClicked = function() {
         alert("Button 1 click handled in controller");
     }
 
-});
+}]);
 
 
 //Option 2 (referencing StudentApp directly ) ; Note: When needed to reference a module we dont pass 2nd param to angular.module(.....) i.e array for dependency. It's needed only during creating a module
-angular.module('StudentApp').controller('StudentController2', function($scope) {
+angular.module('StudentApp').controller('StudentController2', ['$scope', function($scope) {
     $scope.intro = "Option2";
 
     $scope.btnClicked = function() {
         alert("Button 2 click handled in controller");
     }
 
-});
+}]);
 
 // create a IIFE and concentrate on building logic first , and then pass that controller function into controller(.......) 
 (function() {
 
-    var xx = function($scope) {
+    var xx = function($scope) { //dealing with JS minifiers, coz if they minify $scope (to 's', say) so everything would break
         $scope.intro = "Option3";
 
         $scope.btnClicked = function() {
@@ -34,6 +34,6 @@ angular.module('StudentApp').controller('StudentController2', function($scope) {
         }
     }
 
-    angular.module('StudentApp').controller("StudentController3", xx);
+    angular.module('StudentApp').controller("StudentController3", ['$scope', xx]); //dealing with JS minifiers, pass the parameters needed by the controllern funtions above, as comma seperated strings's since minifiers wont touch string. Order is important , obviously. 
 
 }());
