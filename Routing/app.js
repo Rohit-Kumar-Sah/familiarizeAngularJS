4805002869ca26fa74dd9c88c50c3c121b76b725
Rohit Kumar Sah <rohitsah710@gmail.com>
2021-08-08 11:17:13 +0530

    configuring route

--- Routing/app.js
+++ Routing/app.js
@@ -1,7 +1,10 @@
 (
     function() {
         angular.module('Subjects', ['ngRoute']); //adding ngRoute dependency to module
-    }
 
+        angular.module('Subjects').config(function($routeProvider) { //$routeProvider automatically injected by $routeProvider
+            $routeProvider.when('/', { controller: '........', templateUrl: '......' });
+        });
+    }
 
 )(); //Using IIFE so that a global variable need not be created for represeting module, hence other developers cannot accidently step on it
